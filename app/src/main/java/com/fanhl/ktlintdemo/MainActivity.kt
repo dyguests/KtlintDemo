ce1726feeda81f3429c714c5fe832f00f0c92ab2
fanhl <fanhonglin@klicen.com>
2018-08-03 10:53:53 +0800

    错误代码测试

--- app/src/main/java/com/fanhl/ktlintdemo/MainActivity.kt
+++ app/src/main/java/com/fanhl/ktlintdemo/MainActivity.kt
@@ -20,4 +20,81 @@ class MainActivity : AppCompatActivity() {
 
     public val a_BBBBB =0x000
     ;
+
+    ///这里测试ktlint
+    val a_AA=1
+    ///这里测试ktlint
+    val BCD=2
+    ///这里测试ktlint
+     val ______a=3
+    ///这里测试ktlint
+val TAB测试=4
+    ///这里测试ktlint
+val D   =   5
+    ///这里测试ktlint
+val eEeE______E___= "asdf"
+    ///这里测试ktlint
+val 这是KTLINT测试 =
+    ///这里测试ktlint
+    1
+    ///这里测试ktlint
+    companion object
+
+    {
+
+    }
+    ///这里测试ktlint
+
+    class __a{
+
+    }
+    ///这里测试ktlint
+
+    class  bbb constructor( val  a:Int)
+    ///这里测试ktlint
+
+
+
+    val g="g"
+
+
+
+    ///这里测试ktlint
+    ///这里测试ktlint
+    /*这里测试ktlint
+    ///这里测试ktlint
+    *///这里测试ktlint
+    ///这里测试ktlint
+    /** //这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    ///这里测试ktlint
+    *///这里测试ktlint
 }
