cd1ece6940a3ca31dab4265b9773436fe2e877d6
root <xuchenhui@klicen.com>
2018-08-03 10:38:02 +0800

    test kt

--- app/src/androidTest/java/com/fanhl/ktlintdemo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/fanhl/ktlintdemo/ExampleInstrumentedTest.kt
@@ -19,6 +19,9 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
+
+
+
         assertEquals("com.fanhl.ktlintdemo", appContext.packageName)
 
 
